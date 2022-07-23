649a22fd251f401e08ceff40931b38bf1d746b6e
khushalikotadiya <khushalikotadiya67@gmail.com>
2022-07-23 01:08:25 -0700

    commit

---
+++ module2.q13.py
@@ -0,0 +1,7 @@
+'''
+write a python program to count the number of characters (character frequency) in a string.
+
+'''
+
+s=("tops technologies")
+print(s.count("s"))
---
+++ module2.q15.py
@@ -0,0 +1,11 @@
+'''
+write a python program to count occurrences of a substring in a ststring.
+'''
+
+
+
+
+str1="this dress looks good;you have good taste in clothes."
+substr="good"
+count1=str1.count(substr)
+print(count1)
